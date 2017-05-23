6372661e256a7b7056c5667b44f72f144851ea3c
Vintcient <tjaustin1890@gmail.com>
2017-05-22 22:47:59 -0400

    Corrected syntax on lines 3+4. Finished code. Quiz will ask three questions and store and print the number of correct answers in a string upon completion.

--- arrayQuiz.js
+++ arrayQuiz.js
@@ -1,7 +1,7 @@
 var quizQuestions = [
   ['What is the numerical form of two hundred-thirty seven?', 237],
-  ['How many sides are on a coneventional die? (One of a pair of dice)' 6],
-  ['What is the square root of 144?' 12]
+  ['How many sides are on a conventional die? (One of a pair of dice)', 6],
+  ['What is the square root of 144?', 12]
 ];
 var correctAnswers = 0;
 var question;
@@ -17,4 +17,10 @@ for (var i = 0; i < quizQuestions.length; i += 1) {
   question = quizQuestions[i][0];
   answer = quizQuestions[i][1];
   response = parseInt(prompt(question));
+  if (response === answer) {
+   correctAnswers += 1; 
+  }
 }
+
+html = "You got " + correctAnswers + " question(s) right!";
+print(html);
